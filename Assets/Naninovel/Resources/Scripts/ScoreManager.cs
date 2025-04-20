122de0c74883c6ba1790c588f76b8e8419b2e60c
LazyShake <sashaoligator@gmail.com>
2025-04-21 00:08:04 +0700

    Added score manager and it's UI

---
+++ Assets/Naninovel/Resources/Scripts/ScoreManager.cs
@@ -0,0 +1,36 @@
+using Naninovel;
+using UnityEngine;
+
+public class ScoreManager : IEngineService
+{
+    private int score;
+    private ScoreUI scoreUI;
+
+    public int Score => score;
+
+    public Naninovel.UniTask InitializeServiceAsync ()
+    {
+        // Попробуем найти компонент UI в сцене
+        scoreUI = Object.FindObjectOfType<ScoreUI>();
+        UpdateUI();
+        return Naninovel.UniTask.CompletedTask;
+    }
+
+    public void ResetService () => score = 0;
+
+    public void DestroyService () {}
+
+    public void AddScore (int value)
+    {
+        score += value;
+        UpdateUI();
+    }
+
+    public int GetScore () => score;
+
+    private void UpdateUI ()
+    {
+        if (scoreUI != null)
+            scoreUI.SetScore(score);
+    }
+}
